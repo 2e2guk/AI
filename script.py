301c542396c1d15bd4ba9249415c8ebc33828612
이강욱 <90788409+2e2guk@users.noreply.github.com>
2024-11-30 21:55:39 +0900

    Create script.py

---
+++ script.py
@@ -0,0 +1,5 @@
+import os
+print("--------------------Running LR Model--------------------")
+os.system("python3 LR.py")
+print("--------------------Running MLP Model--------------------")
+os.system("python3 MLP.py")
