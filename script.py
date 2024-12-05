96a195030080810e7e2328a217b13738f565b206
이강욱 <90788409+2e2guk@users.noreply.github.com>
2024-12-06 05:21:57 +0900

    Delete script.py

--- script.py
+++
@@ -1,5 +0,0 @@
-import os
-print("--------------------Running LR Model--------------------")
-os.system("python3 LR.py")
-print("--------------------Running MLP Model--------------------")
-os.system("python3 MLP.py")
